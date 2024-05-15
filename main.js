d0865e414e34d2089998c3e18a966d5ddbe03095
tiposamuel <tipo-samuel@hotmail.com>
2024-05-15 03:23:44 -0400

    Optimized code for multiple infinite scroll sections

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
-const scrollers = document.querySelectorAll('.scroller')
+const marqueeHTML = (selector, innerSelector) => {
+  // selecciona todos los elementos del DOM que coinciden con el parametro 'selector'
+  const elements = document.querySelectorAll(selector)
 
-const addAnimation = () => {
-  scrollers.forEach((scroller) => {
-    scroller.setAttribute('data-animated', true)
+  // itera sobre cada elemento del DOM seleccionado
+  elements.forEach((element) => {
+    // establece un atributo personalizado en verdadero para cada elemento
+    element.setAttribute('data-animated', true)
 
-    const scrollerInner = scroller.querySelector('.scroller-inner')
-    const scrollerContent = Array.from(scrollerInner.children)
+    // dentro de cada elemento del DOM seleccionado busca cualquier hijo que coincida con el parametro 'innerSelector'
+    const innerElement = element.querySelector(innerSelector)
 
-    scrollerContent.forEach((item) => {
+    // toma todos los elementos hijos dentro de 'innetSelector' y los convierte en un arreglo
+    const content = Array.from(innerElement.children)
+
+    // itera sobre cada elemento del array
+    content.forEach((item) => {
+      // clona cada elemento del array
       const duplicateItem = item.cloneNode(true)
+
+      // establece el atributo 'aria-hidden' a cada nuevo elemento clonado
+      // esto se hace para indicar a las tecnologías de asistencia que este elemento está oculto visualmente
       duplicateItem.setAttribute('aria-hidden', true)
-      scrollerInner.appendChild(duplicateItem)
+
+      // agrega el elemento clonado al final del elemento 'innerElement' duplicando su contenido
+      innerElement.appendChild(duplicateItem)
     })
   })
 }
 
 if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-  addAnimation()
+  marqueeHTML('.scroller', '.scroller-inner')
+  marqueeHTML('.marquee', '.marquee-inner')
 }
